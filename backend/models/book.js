5028763428b9b234d9242bb8092f427cb965a773
artemychych <artemychmain@gmail.com>
2026-03-17 20:59:03 +0900

    added documentation to books.js

--- backend/models/book.js
+++ backend/models/book.js
@@ -32,4 +32,9 @@ module.exports = (sequelize, DataTypes) => {
     },
   );
   return Book;
+  /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
 };
