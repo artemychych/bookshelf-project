793cecc877b68b366d2f5a40a4b0cf02e2953b01
artemychych <artemychmain@gmail.com>
2026-03-17 21:03:44 +0900

    переделал документацию

--- backend/models/book.js
+++ backend/models/book.js
@@ -36,5 +36,7 @@ module.exports = (sequelize, DataTypes) => {
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
+     * Крутая модель очень жеская 
+     * Попытка
      */
 };
