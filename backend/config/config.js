111419ed76f82b25e1dcf69dbc0736f071a1d5d9
artemychych <artemychmain@gmail.com>
2026-04-08 11:31:58 +0900

    added test config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -10,9 +10,21 @@ module.exports = {
     dialect: "postgres",
   },
   test: {
-    // ... test environment configuration ...
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    logging: false,
   },
   production: {
-    // ... production environment configuration ...
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    logging: false,
   },
 };
